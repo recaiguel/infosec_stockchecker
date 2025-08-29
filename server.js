45104aa9f95081f27dda71b0c05976e8cf92fbee
recaiguel <recaiguel@gmail.com>
2025-08-29 03:18:48 +0000

    added helmet as content security policy

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self"],
+    scriptSrc: ["'self"],
+    styleSrc: ["'self"]
+  }
+}))
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
